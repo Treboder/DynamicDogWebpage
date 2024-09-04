9cca6d2478fdccb32a397124162cc6728fcf501b
Treboder <robert.kohn@gmx.net>
2024-09-04 20:53:39 +0200

    Fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // js health check
-document.getElementById("demo").innerHTML = "Healt check says: JavaScript is working!";
+document.getElementById("demo").innerHTML = "Health check says: JavaScript is working!";
 
 // API call
 function callAPI() {
