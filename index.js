aa4289af5ebfd6a09eab622d4546439f230a823d
Treboder <robert.kohn@gmx.net>
2024-09-04 22:43:59 +0200

    Refactoring complete

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // js health check
 document.getElementById("demo").innerHTML = "Health check says: JavaScript is working!";
 
+// Dog API endpoint (for random dog images)
 const apiUrl = 'https://dog.ceo/api/breeds/image/random';
 
-// call API
+//////////////////////////////////////////////////////////////////////////
+// Simple API call returning json response
+//////////////////////////////////////////////////////////////////////////
+
 function callAPI() {
     // Make an API request
     fetch(apiUrl)
@@ -18,15 +22,29 @@ function callAPI() {
         });
 }
 
-// show predefined image
+//////////////////////////////////////////////////////////////////////////
+// Show PREDEFINED image from Dog API
+//////////////////////////////////////////////////////////////////////////
+
 function displayPredefinedImage() {
+    //clearPredefinedImageElement()
     var img = new Image();
     img.src = "https://images.dog.ceo/breeds/setter-irish/n02100877_602.jpg"
     img_home.appendChild(img);
 }
 
-// use url in image tag
+function clearPredefinedImageElement() {
+    while (img_home.firstChild) {
+        img_home.removeChild(img_home.firstChild);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Show RANDOM image from Dog API
+//////////////////////////////////////////////////////////////////////////
+
 async function displayRandomImage() {
+    clearRandomImageElement()
     const imageUrl = await fetchImageUrl();
     if (imageUrl) {
         var img = new Image();
@@ -37,7 +55,12 @@ async function displayRandomImage() {
     }
 }
 
-// get url from API
+function clearRandomImageElement() {
+    while (api_image.firstChild) {
+        api_image.removeChild(api_image.firstChild);
+    }
+}
+
 async function fetchImageUrl() {
     try {
         const response = await fetch(apiUrl);
