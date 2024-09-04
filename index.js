f1763e0f130981a52ace743efaaccad0a941bbdb
Treboder <64498640+Treboder@users.noreply.github.com>
2024-09-04 14:42:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 document.getElementById("demo").innerHTML = "JavaScript is working!";
+
+function callAPI() {
+    // Make an API request
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            // Display the result
+            document.getElementById('result').innerHTML = JSON.stringify(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
