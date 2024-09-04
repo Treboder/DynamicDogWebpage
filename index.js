ee0101300e5f22c9578d0255d84d15e24bf0bd4b
Treboder <robert.kohn@gmx.net>
2024-09-04 22:25:22 +0200

    better, but still not good

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // js health check
 document.getElementById("demo").innerHTML = "Health check says: JavaScript is working!";
 
-// API call
+const apiUrl = 'https://dog.ceo/api/breeds/image/random';
+
+// call API
 function callAPI() {
     // Make an API request
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             // Display the result
@@ -17,41 +19,35 @@ function callAPI() {
 }
 
 // show predefined image
-function addImage() {
+function displayPredefinedImage() {
     var img = new Image();
     img.src = "https://images.dog.ceo/breeds/setter-irish/n02100877_602.jpg"
     img_home.appendChild(img);
 }
 
-// Beispiel-API-Endpunkt
-const apiUrl = 'https://dog.ceo/api/breeds/image/random';
+// use url in image tag
+async function displayRandomImage() {
+    const imageUrl = await fetchImageUrl();
+    if (imageUrl) {
+        var img = new Image();
+        img.src = imageUrl;
+        api_image.appendChild(img);
+    } else {
+        document.getElementById('api-image').alt = 'Failed to load image from Dog API';
+    }
+}
 
-// Funktion zum Abrufen des Bild-URLs von der API
+// get url from API
 async function fetchImageUrl() {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        // Nehmen wir an, das Bild-URL ist im Feld 'imageUrl'
-        //return JSON.stringify(data.message);
         return data.message;
     } catch (error) {
-        console.error('Fehler beim Abrufen des Bild-URLs:', error);
+        console.error('Failed to call Dog API:', error);
         return null;
     }
 }
 
-// Funktion zum Setzen des Bild-URLs in das img-Tag
-async function displayImage() {
-    const imageUrl = await fetchImageUrl();
-    if (imageUrl) {
-        var img = new Image();
-        //img.src = "https://images.dog.ceo/breeds/setter-irish/n02100877_602.jpg"
-        img.src = imageUrl;
-        api_image.appendChild(img);
-    } else {
-        document.getElementById('api-image').alt = 'Bild konnte nicht geladen werden';
-    }
-}
 
-// Bild anzeigen
-displayImage();
+
